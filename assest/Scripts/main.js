101382793bc0826329fb7784f875039348775507
Mohamed Hamdy <139185513+Mohamed2247@users.noreply.github.com>
2024-06-02 14:03:26 +0300

    Update main.js

--- assest/Scripts/main.js
+++ assest/Scripts/main.js
@@ -1,11 +1,7 @@
-document
-  .getElementById("loginForm")
-  .addEventListener("submit", function (event) {
+document.getElementById("loginForm").addEventListener("submit", function (event) {
     event.preventDefault();
-
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
-
     if (username === "admin" && password === "admin123") {
       localStorage.setItem("isLoggedIn", "true");
       window.location.href = "configuration.html";
